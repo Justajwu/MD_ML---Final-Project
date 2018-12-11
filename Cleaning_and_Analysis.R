f109c055f892cd947d884b505c320af9ad675952
James Wu <jameswu0816@yahoo.com>
2018-12-11 14:22:29 -0500

    12/11/18 Update

--- Cleaning_and_Analysis.R
+++ Cleaning_and_Analysis.R
@@ -1,20 +1,46 @@
-source("NewAPIScript.R")
+#source("NewAPIScript.R")
+require(tidyverse)
 require(lubridate)
 require(ggplot2)
+require(tidytext)
+
+### Read in data ###
+raw_df <- read_csv("articles_df.csv")
+
 
 ### Cleaning ###
+articles_df <- raw_df
+
+
+### Adding features ###
+articles_df <- articles_df %>%
+  mutate(date = date(publishedAt),
+         month = month(date),
+         weekday = weekdays(date))
 
 
 ### Exploratory ###
 
+## Number of articles per news source in the month
+articles_df %>%
+  ggplot() +
+  geom_bar(aes(x = source_name)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
 ## Number of articles by Newssource by time
+newssource <- c("abc-news","cnn","bbc-news","breitbart-news","fox-news","the-hill","the-huffington-post",
+                "the-new-york-times","the-american-conservative")
+
 articles_df %>% 
-  count(source_id,date = date(publishedAt)) %>%
-  right_join(expand(.,source_id = newssource,date)) %>%
-  mutate(n = replace_na(n,0) ) %>%
-  ggplot(aes(y = n, x = date, color = source_id)) +
-  geom_line() +
+  ggplot(aes(x = factor(date))) +
+  geom_bar(aes(fill = source_name)) +
   labs(x = "Date Published",
        y = "Number of Related Articles Published",
-       color = "")
-  
+       color = "") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+### Comparison of words ###
+
+
+
--- NewAPIScript.R
+++ NewAPIScript.R
@@ -1,21 +1,25 @@
+require(lubridate)
+require(tidyverse)
+
 apiKey_retriever <- function( api_file = 'apiKey.cred' ){
   apiKey <- readChar( api_file, file.info(api_file)$size )
   apiKey <- gsub( pattern = "[^a-zA-Z0-9]", replacement = '', x = apiKey )
   return( apiKey )
 }
 
+tempapi <- "ccd45384f7904e07bca1816d7ef5a4ce"
 
-everything_api_call_creator <- function( q = 'mueller',
+everything_api_call_creator <- function( q = NA,
                                          sources = NA,
                                          domains = NA,
                                          excludeDomains = NA,
                                          from = NA, # e.g. '2018-01-01T00:00:00'
                                          to = NA, # e.g. '2018-10-25T11:00:00'
                                          language = 'en',
-                                         sortBy = NA, #e.g. 'popularity'
-                                         pageSize = NA,
+                                         sortBy = 'popularity', #e.g. 'popularity'
+                                         pageSize = 30,
                                          page = NA,
-                                         apiKey = apiKey_retriever() ){
+                                         apiKey = tempapi ){
   require( utils )
   api_call <- 'https://newsapi.org/v2/everything'
   first_set <- F
@@ -145,25 +149,51 @@ retrieve_news_json <- function( api_call_creator = everything_api_call_creator,
 #acons_articles <- retrieve_news_json(sources = "the-american-conservative")
 
 #News Sources
-newssource <- c("abc-news","cnn","bbc-news","breitbart-news","fox-news","the-hill","the-huffington-post",
-                "the-new-york-times","the-american-conservative")
+newssource <- c("abc-news","cnn","bbc-news","breitbart-news","fox-news","the-hill","the-huffington-post", "the-new-york-times","the-american-conservative")
 
 #Function to make each pull's data.frame compatible to row bind
 change.source <- function(df){
   return(cbind(data.frame(source_id = df$articles$source$id,source_name = df$articles$source$name),df$articles[,-1]))
 }
 
-#Create the data tibble
-require(dplyr)
-articles_df <- tibble()
-for(source in newssource){
-  articles <- retrieve_news_json(sources = source)
-  if(articles$totalResults != 0)  articles_df <- bind_rows(articles_df,change.source(articles))
+##Create the data tibble
+#Set timeout
+options(timeout = 4000000)
+#News API only gathers maximum of 100 articles for each news source per request. 
+#Write function to iteratively grab up to n day's worth of 30 articles per source. Recommend doing around 10
+#day each time
+gather_articles <- function(ndays = 10){
+  today <- '2018-12-10T23:59:59'
+  earlytoday <- '2018-12-10T00:00:00'
+  output <- tibble()
+  for(ntry in 1:ndays){
+    articles_df <- tibble()
+    for(source in newssource){
+      articles <- retrieve_news_json(sources = source,from = earlytoday,to = today)
+      if(articles$totalResults != 0)  articles_df <- bind_rows(articles_df,change.source(articles))
+      Sys.sleep(5)
+    }
+    articles_df <- articles_df %>%
+      mutate(publishedAt = ymd_hms(publishedAt))
+    
+    output <- bind_rows(output,articles_df)
+    
+    t <- ymd_hms(today) - hms("24:00:00")
+    today = paste0(year(t),"-",sprintf("%02d", month(t)),"-",sprintf("%02d", day(t)),"T",sprintf("%02d", hour(t)),":",sprintf("%02d", minute(t)),":",sprintf("%02d", second(t)))
+    
+    et <- ymd_hms(earlytoday) - hms("24:00:00")
+    earlytoday <- paste0(year(et),"-",sprintf("%02d", month(et)),"-",sprintf("%02d", day(et)),"T",sprintf("%02d", hour(et)),":",sprintf("%02d", minute(et)),":",sprintf("%02d", second(et)))
+    Sys.sleep(60)
+  }
+  output
 }
 
-require(lubridate)
+articles_df <- gather_articles()
 
-articles_df <- articles_df %>%
-  mutate(publishedAt = ymd_hms(publishedAt))
-
-write_csv(articles_df,"articles_df.csv")
+#write_csv(articles_df,"articles_df_20-30days.csv")
+#art1 <- read_csv("articles_df_10days.csv")
+#art2 <- read_csv("articles_df_10-20days.csv")
+#art3 <- read_csv("articles_df_20-30days.csv")
+#
+#articles_df <- bind_rows(art1,art2,art3)
+#write_csv(articles_df,"articles_df.csv")
